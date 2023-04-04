1f783679dddce70fba4a3988ae754f95a6a48958
bautykamijo <bkamijo@udesa.edu.ar>
2023-04-04 17:44:10 -0300

    primer ruta y css

---
+++ proyecto1-grupo5/controllers/productController.js
@@ -0,0 +1,11 @@
+const productController = {
+
+    listaProductos : function (req, res) {
+        return res.send('Cambiar esto por un render con info del producto');
+    }
+
+
+
+}
+
+module.exports = productController;
