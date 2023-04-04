374fcf6c9e4d6a0d7fdcb3296a6e529c5f12b15f
bautykamijo <bkamijo@udesa.edu.ar>
2023-04-04 18:11:51 -0300

    algo

---
+++ proyecto1-grupo5/controllers/loginController.js
@@ -0,0 +1,11 @@
+const loginController = {
+
+    ingresar : function (req, res) {
+        return res.send('Cambiar esto por un render con info del producto');
+    }
+
+
+
+}
+
+module.exports = loginController;
--- proyecto1-grupo5/routes/product.js
+++ proyecto1-grupo5/routes/product.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const productController = require('../controllers/productController');
 
-router.get('/detalleproducto/:id', productController.listaProductos);
+router.get('/producto/:id', productController.listaProductos);
 
 module.exports = router;
 
