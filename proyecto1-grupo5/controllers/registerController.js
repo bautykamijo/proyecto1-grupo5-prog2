d85c689ef2352991ddc17264f16368e0e789ac63
ArielAzambuja <aazambuja@udesa.edu.ar>
2023-04-07 21:29:13 -0300

    cabeza quemada

--- proyecto1-grupo5/controllers/registerController.js
+++ proyecto1-grupo5/controllers/registerController.js
@@ -1,9 +1,23 @@
-const registerController={
-    registro: function (req,res) {
-        res.send("aca me mataste")
-        
-    }
+// Importar los módulos necesarios
+const express = require('express');
+const router = express.Router();
+router.get("/registro", function(req,res){
+const registerController ={
+    registro : { 
+    email: req.query.email,
+    usuario : req.query.usuario,
+    contrasenia : req.query.contrasenia,
+    fechaNacimiento : req.query.fechaNacimiento,
+    nroDocumento : req.query.nroDocumento}
+  
+
+    
+    
+
+    }})
+    res.render('confirmacionRegistro', {email: email, usuario: usuario, fechaNacimiento: fechaNacimiento, nroDocumento: nroDocumento});
+;
 
 
-}
-module.exports=registerController
+
+  module.exports = router;
